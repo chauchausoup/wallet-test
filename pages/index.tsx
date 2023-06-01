6da81eac146bdc49104861d3de76abc21d2d6831
Krishna Dhakal <7krishna7dhakal7@gmail.com>
2023-06-01 14:05:16 +0545

    initial form setup

---
+++ pages/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Form from '../src/components/form'
+
+const HomePage = () => {
+	return (
+		<Form />
+	);
+};
+
+export default HomePage;
---
+++ src/components/form.tsx
@@ -0,0 +1,49 @@
+"use client"
+
+import { useState } from 'react';
+
+const ConversionRate = 3; // Conversion rate: 1 NEP = 3 BUSD
+
+const HomePage: React.FC = () => {
+	const [nepValue, setNepValue] = useState('');
+	const [busdValue, setBusdValue] = useState('');
+
+	const handleNepChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+		const { value } = event.target;
+		setNepValue(value);
+		setBusdValue(value ? (parseFloat(value) * ConversionRate).toString() : '');
+	};
+
+	const handleBusdChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+		const { value } = event.target;
+		setBusdValue(value);
+		setNepValue(value ? (parseFloat(value) / ConversionRate).toString() : '');
+	};
+
+	return (
+		<div className="flex flex-col items-center justify-center h-screen">
+			<h1 className="text-3xl mb-4">Currency Converter</h1>
+			<label className="flex items-center space-x-2">
+				NEP:
+				<input
+					type="number"
+					value={nepValue}
+					onChange={handleNepChange}
+					className="border border-gray-300 rounded px-2 py-1 text-black"
+				/>
+			</label>
+			<br />
+			<label className="flex items-center space-x-2">
+				BUSD:
+				<input
+					type="number"
+					value={busdValue}
+					onChange={handleBusdChange}
+					className="border border-gray-300 rounded px-2 py-1 text-black"
+				/>
+			</label>
+		</div>
+	);
+};
+
+export default HomePage;
