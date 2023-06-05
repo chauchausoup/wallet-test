24b22cc7aacfbd733deb4aa1463a2bd9afe8f5c2
Krishna Dhakal <7krishna7dhakal7@gmail.com>
2023-06-05 13:00:14 +0545

    metamask button issue resolved

--- src/components/AlertModal.tsx
+++ src/components/AlertModal.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import { UserRejectedRequestError } from '@web3-react/injected-connector';
 import { useWeb3React } from '@web3-react/core';
 import useMetaMaskOnboarding from '../lib/hooks/useMetaMaskOnboarding';
@@ -13,29 +13,32 @@ interface AlertModalProps {
 export default function AlertModal({
   closeModal,
 }: AlertModalProps): JSX.Element {
-  const { activate, account, setError } = useWeb3React();
+  const { account, activate } = useWeb3React();
 
-  const activateWallet = async () => {
-    await activate(injected, undefined, true).catch((error: Error) => {
+  const connectMetaMask = useCallback(async () => {
+    try {
+      await activate(injected, undefined);
+    } catch (error) {
       if (error instanceof UserRejectedRequestError) {
+        throw new Error('Wallet connection request was rejected.');
       } else {
-        setError(error);
+        throw error;
       }
-    });
-  };
+    }
+  }, [activate]);
 
   const { isMetaMaskInstalled, isWeb3Available, startOnboarding } =
     useMetaMaskOnboarding();
 
-  if (typeof account !== 'string') {
+  if (!account) {
     return (
       <div>
         <p className="m-5 text-red-500">
-          No wallet detected. Please follow below operation to continue.
+          No wallet detected. Please follow the steps below to continue.
         </p>
         {isWeb3Available ? (
           <Button
-            onClickHandler={activateWallet}
+            onClickHandler={connectMetaMask}
             buttonContent={
               isMetaMaskInstalled ? 'Connect to MetaMask' : 'Connect to Wallet'
             }
@@ -43,10 +46,10 @@ export default function AlertModal({
         ) : (
           <Button
             onClickHandler={startOnboarding}
-            buttonContent={`Install Metamask`}
+            buttonContent="Install MetaMask"
           />
         )}
-        <Button onClickHandler={closeModal} buttonContent={`Close`} />
+        <Button onClickHandler={closeModal} buttonContent="Close" />
       </div>
     );
   }
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,14 +1,20 @@
 import React, { ReactNode, MouseEventHandler } from 'react';
 
 interface ButtonProps {
-	buttonContent: ReactNode;
-	onClickHandler: MouseEventHandler<HTMLButtonElement>;
+  buttonContent: ReactNode;
+  onClickHandler: MouseEventHandler<HTMLButtonElement>;
 }
 
-export default function Button({ buttonContent, onClickHandler }: ButtonProps): JSX.Element {
-	return (
-		<button className='border border-gray-300 rounded px-6 py-3 m-10 text-black' onClick={onClickHandler}>
-			{buttonContent}
-		</button>
-	);
+export default function Button({
+  buttonContent,
+  onClickHandler,
+}: ButtonProps): JSX.Element {
+  return (
+    <button
+      className="border border-gray-300 rounded px-6 py-3 m-10 text-black"
+      onClick={onClickHandler}
+    >
+      {buttonContent}
+    </button>
+  );
 }
